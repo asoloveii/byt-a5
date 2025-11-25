2caa708c8e8462dae45cdb083638c9e5772c1cdb
Arseni Salavei <arsenisoloveii@gmail.com>
2025-11-25 17:31:35 +0100

    assignment 5

---
+++ BYT_Assignment5/Adapter/EmployeeAdapter.cs
@@ -0,0 +1,25 @@
+﻿// In case you need some guidance: https://refactoring.guru/design-patterns/adapter
+namespace DesignPattern.Adapter
+{
+    public class EmployeeAdapter : ITarget
+    {
+        private readonly BillingSystem thirdPartyBillingSystem = new();
+
+        public void ProcessCompanySalary(string[,] employeesArray)
+        {
+            List<Employee> employees = new();
+
+            for (int i = 0; i < employeesArray.GetLength(0); i++)
+            {
+                int id = int.Parse(employeesArray[i, 0]);
+                string name = employeesArray[i, 1];
+                string designation = employeesArray[i, 2];
+                decimal salary = decimal.Parse(employeesArray[i, 3]);
+
+                employees.Add(new Employee(id, name, designation, salary));
+            }
+
+            thirdPartyBillingSystem.ProcessSalary(employees);
+        }
+    }
+}
---
+++ BYT_Assignment5/Observer/Observer.cs
@@ -0,0 +1,27 @@
+﻿// In case you need some guidance: https://refactoring.guru/design-patterns/observer
+
+namespace DesignPattern.Observer
+{
+    public class ConcreteObserver(string userName) : IObserver
+    {
+        public string UserName { get; set; } = userName;
+
+        // Step 3: Register observer with subject
+        public void AddSubscriber(Subject subject)
+        {
+            subject.RegisterObserver(this);
+        }
+
+        // Step 4: Remove observer from the subject
+        public void RemoveSubscriber(Subject subject)
+        {
+            subject.RemoveObserver(this);
+        }
+
+        // Step 5: Get updates from subject
+        public void Update(string availability)
+        {
+            Console.WriteLine($"Hello {UserName}, the product you are watching is now {availability}.");
+        }
+    }
+}
